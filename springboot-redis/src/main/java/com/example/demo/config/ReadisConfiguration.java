38e219bcc418cd7321f9ca351e56e12f4f4e01b3
周聪 <@zc>
2017-07-18 23:33:16 +0800

    自定义Key 我们打开RedisConfiguration配置类，添加继承CachingConfigurerSupport类，并重写方法keyGenerator()

--- springboot-redis/src/main/java/com/example/demo/config/ReadisConfiguration.java
+++ springboot-redis/src/main/java/com/example/demo/config/ReadisConfiguration.java
@@ -1,7 +1,13 @@
 package com.example.demo.config;
 
+import java.lang.reflect.Method;
+import java.security.Provider;
+
+
 import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheManager;
@@ -17,7 +23,40 @@ import org.springframework.data.redis.core.RedisTemplate;
  */
 @Configuration
 @EnableCaching//注解来开启我们的项目支持缓存，我们在配置类内添加了方
-public class ReadisConfiguration {
+public class ReadisConfiguration extends CachingConfigurerSupport{
+
+	@Override
+	public KeyGenerator keyGenerator() {
+		        return new KeyGenerator(){
+
+					@Override
+					public Object generate(Object o, Method method, Object... objects) {
+						//格式化缓存key字符串
+						StringBuilder sb=new StringBuilder();
+						//追加类名
+						sb.append(o.getClass().getName());
+						//追加方法名
+						sb.append(method.getName());
+						//遍历参数并且追加
+						for (Object obj : objects) {
+							sb.append(obj.toString());
+						}
+						System.out.println("调用Redis缓存Key:"+sb.toString());
+						        return sb.toString();  
+						    
+					}
+		        	
+		        };  
+		    
+	}
+
+	/**
+	 * 自定义生成key的规则
+	    * @param keyGenSpi
+	    * @param provider
+	    * @param algorithm
+	 */
+
 	
 	/**
 	 * 
@@ -30,11 +69,11 @@ public class ReadisConfiguration {
 	    * @returnType:@return CacheManager  
 		* @copyright:zong
 	 */
-	@Bean
+/*	@Bean
 	public CacheManager cacheManager(RedisTemplate redisTemplate){
 		return new RedisCacheManager(redisTemplate) ;
 		}
-
+*/
 }
   
     
