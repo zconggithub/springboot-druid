b121f8da337e44d233fcf6130d743c83fa73e282
zconggithub <zc2632652610@sina.com>
2017-07-18 23:39:47 +0800

    Update SpringbootShiroApplication.java

--- springboot-redis/src/main/java/com/example/demo/SpringbootShiroApplication.java
+++ springboot-redis/src/main/java/com/example/demo/SpringbootShiroApplication.java
@@ -9,6 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootShiroApplication {
 
 	public static void main(String[] args) {
+		//demo：redis+druid
+		
 		SpringApplication.run(SpringbootShiroApplication.class, args);
 	}
 }
