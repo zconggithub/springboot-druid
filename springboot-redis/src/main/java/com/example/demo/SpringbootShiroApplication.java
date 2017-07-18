52cf573a6db353b03e805db3d656cb5f37d3383e
周聪 <@zc>
2017-07-18 23:44:01 +0800

    修改demo注释

--- springboot-redis/src/main/java/com/example/demo/SpringbootShiroApplication.java
+++ springboot-redis/src/main/java/com/example/demo/SpringbootShiroApplication.java
@@ -9,8 +9,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootShiroApplication {
 
 	public static void main(String[] args) {
+
 		//demo：redis+druid
-		
+
 		SpringApplication.run(SpringbootShiroApplication.class, args);
 	}
 }
